fcacbd46de4badf75e178fdcf35ec399b6746b62
Marcel Cornett <marcornett@gmail.com>
2020-08-10 16:18:13 -0400

    Created components folder with: email, footer, head, nav, and social files.

---
+++ src/components/routes/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-dom'
+import { Home } from '../../screens/Home'
+
+export const Routes = () => {
+    return (
+        <Home />
+    )
+}
